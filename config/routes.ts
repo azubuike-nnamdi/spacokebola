a0d302565037831fab4122616b334dac8b29ac8a
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-31 10:56:09 +0100

    feat: constant routes :zap:

---
+++ config/routes.ts
@@ -0,0 +1,16 @@
+const HOME_URL = "/";
+const ABOUT_URL = "/about";
+const ANNOUNCEMENTS_URL = "/announcements";
+const EVENTS_URL = "/events";
+const GALLERY_URL = "/gallery";
+const CONTACT_URL = "/contact";
+const BRANCHES_URL = "/branches";
+export {
+  HOME_URL,
+  ABOUT_URL,
+  ANNOUNCEMENTS_URL,
+  EVENTS_URL,
+  GALLERY_URL,
+  CONTACT_URL,
+  BRANCHES_URL,
+};
