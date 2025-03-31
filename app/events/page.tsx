f66e05c87ee580426afa75e7439b62b7524ce355
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-03-31 17:09:24 +0100

    chore: include metadata :zap:

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,11 @@
 import EventDashboard from "@/components/event/event-dashboard";
 import { PageTransition } from "@/components/ui/page-transition";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Events",
+  description: "View our events and activities",
+}
 
 export default function Events() {
   return (
