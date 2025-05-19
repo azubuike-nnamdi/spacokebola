8386f0d74cfdd354568c04df66b910e83931c450
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-05-19 11:05:44 +0100

    feat: admin dashboard layout :zap:

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { AppSidebar } from "@/components/common/app-sidebar";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { Header } from "@/components/common/header";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import "../../globals.css";
 
@@ -12,10 +13,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main>
-        <SidebarTrigger />
+      <main className="w-full flex-1 overflow-auto p-4 md:p-6">
+
+        <Header />
         {children}
       </main>
+
     </SidebarProvider>
   )
 }
