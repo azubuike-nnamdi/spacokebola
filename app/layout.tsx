2c12f0e9722d1cdff83b875c7e1c9309349146c2
Nnamdi Azubuike <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-06-27 07:35:53 +0100

    chore: config fonts :zap: (#5)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,110 @@
 import TanstackProvider from "@/context/tanstack-provider";
 import { ThemeProvider } from "@/context/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const montserrat = localFont({
+  variable: '--montserrat',
+  src: [
+    {
+      path: './fonts/Montserrat-ExtraLight.ttf',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Montserrat-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Montserrat-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Montserrat-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Montserrat-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/Montserrat-ExtraBold.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+})
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const openSans = localFont({
+  variable: "--open-sans",
+  src: [
+    {
+      path: './fonts/OpenSans-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: './fonts/OpenSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: './fonts/OpenSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: './fonts/OpenSans-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+    {
+      path: './fonts/OpenSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: './fonts/OpenSans-ExtraBold.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: './fonts/OpenSans-ExtraBoldItalic.ttf',
+      weight: '900',
+      style: 'italic',
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -32,7 +124,8 @@ export default function RootLayout({
         <link rel="icon" href="/assets/img/spac-logo.png " />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.variable} ${openSans.variable} antialiased`}
+        suppressHydrationWarning
       >
         <TanstackProvider>
           <ThemeProvider
