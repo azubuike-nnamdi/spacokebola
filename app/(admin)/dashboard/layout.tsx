590cb938c362b8f148b933aa7098086e21f53cac
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2025-07-03 22:38:31 +0100

    fix: build error :bug:

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -13,18 +13,20 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full flex-1 overflow-auto p-4 md:p-6">
-        <Header />
-        <SignedIn>
-          {children}
-        </SignedIn>
-        <SignedOut>
-          <div className="flex items-center justify-center min-h-screen">
-            <SignIn afterSignInUrl="/dashboard" />
-          </div>
-        </SignedOut>
-      </main>
+      <div >
+        <AppSidebar />
+        <main className="w-full flex-1 overflow-auto p-4 md:p-6">
+          <Header />
+          <SignedIn>
+            {children}
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center justify-center min-h-screen">
+              <SignIn afterSignInUrl="/dashboard" />
+            </div>
+          </SignedOut>
+        </main>
+      </div>
     </SidebarProvider>
   )
 }
