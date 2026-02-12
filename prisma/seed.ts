90de8e998b903ef2fb7a0524221e66efe5d8ade9
azubuike-nnamdi <35131360+azubuike-nnamdi@users.noreply.github.com>
2026-02-12 14:20:49 +0100

    fix: broken seed file :ant:

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-
-const prisma = new PrismaClient();
+import { prisma } from "../lib/prisma";
 
 async function main() {
   // Create super admin
